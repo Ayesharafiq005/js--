7979c8f793370c99f62ae057f34629aa587c5b6b
Ayesharafiq005 <iamayesharafiq@gmail.com>
2025-07-13 05:37:45 +0000

    modified:   .devcontainer/devcontainer.json 	new file:   01_Basics/02_dataTypes.js 	new file:   01_Basics/03_conversionOperation.js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict "; // treat all js code as newer version 
+
+// alert(3+3) // we are using node.js not browser 
+
+console.log(3+3); 
+console.log(4+5);
+
+let name = "aashi"
+let Id = 838383 
+let isLoggedIn = "false"
+
+// number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness of individual component 
+// node 01_Basics/02_dataTypes.js
+
+// Objects
+
+console.log(typeof "aashi"); // string 
+
+console.log(typeof null); // will give object
+console.log(typeof undefined); // will give undefined 
+
+
+
+
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+//let score = true 
+// let score = "40abc"
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// node 01_Basics/03_conversionOperation.js
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN => cannot be coverted => not a number but NaN's type is a number
+
+// "40" => 40
+// "40abc" => NaN
+//true => 1; false => 0;
+
+// let isLoggedIn = null 
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false 
+// "" => false
+//"aashi" "0" "1" => all true 
+
+let someNumber = 12 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber) 
+console.log(typeof stringNumber); 
+
+
